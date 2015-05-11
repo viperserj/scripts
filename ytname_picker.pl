ede424447a33be67e6ba416b87406a99e138789c
user <user@localhost.localdomain>
2015-05-11 12:46:03 +0300

    youtube script upgraded

--- ytname_picker.pl
+++ ytname_picker.pl
@@ -1,7 +1,4 @@
 #!/usr/bin/perl -pl
-#use IPC::system::Simple qw(capture);
-
-#script
 my $title = `curl --silent $_ | egrep -o '<title>.*<\/title>' | cut -c8- | cut -d '<' -f 1`;
 my $user = `curl --silent $_ | egrep -o '\"\/user\/.*\"' | cut -c8- | cut  -d '\"' -f 1`;
 print "$user$title";
