a1e4434b23e35b65068b0b754b599c490918d47f
user <user@localhost.localdomain>
2015-05-13 12:19:34 +0300

    Forecast - 0.1.1 - minor issues fixed

--- forecast.pl
+++ forecast.pl
@@ -1,9 +1,12 @@
 #!/usr/bin/perl -pls
 #Forecaster - граббер погоды с pogoda.yandex.ru
-#Версия 0.1 alpha
+#Версия 0.1.1 alpha
+#Changelog:
+#0.1 - initial release
+#0.1.1 - Minor fixes :D
 #To-do: скорость и направление ветра (возможно со стрелочками =3); прогноз погоды на несколько дней; ввод города БЕЗ режима прямого ввода (forecast Saratov, к примеру).
 system "curl -silent https://pogoda.yandex.ru/$_ -o \"tmp\"";
-$title = `egrep -o 'title_level_1">.*<\/h1>' ./tmp | cut -c1-16 | cut -d '<' -f 1`;
+$title = `egrep -o 'title_level_1">.*<\/h1>' ./tmp | cut -c16- | cut -d '<' -f 1`;
 $current = `egrep -o '_thermometer_type_now">.[0-9]*&thinsp;°C<\/div>' ./tmp | cut -c24- | cut -d '&' -f 1`;
 $after = `egrep -o '_thermometer_type_after">.[0-9]*<\/div>' ./tmp | cut -c26- | cut -d '<' -f 1`;
 
@@ -13,6 +16,6 @@ $current =~ s/\n/°C/i;
 $comment =~ s/\n//i;
 $after =~ s/\n/°C → /i;
 $after =~ s/\n/°C/i;
-print "$title\nСейчас $current, $comment";
+print "###Ctrl-C to exit###\n\n$title\nСейчас $current, $comment";
 print "Ближайший прогноз: $current → $after";
 exit 0;
