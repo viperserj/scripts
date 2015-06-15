b12711be7973100c81b7604ec6d7a12eb581fdd8
Sergey Shcherbakov <sj@404.pm>
2015-06-15 22:41:15 +0300

    Cleaned repo

--- ytname_picker.pl
+++ ytname_picker.pl
@@ -1,4 +1,11 @@
-#!/usr/bin/perl -pl
-my $title = `curl --silent $_ | egrep -o '<title>.*<\/title>' | cut -c8- | cut -d '<' -f 1`;
-my $user = `curl --silent $_ | egrep -o '\"\/user\/.*\"' | cut -c8- | cut  -d '\"' -f 1`;
-print "$user$title";
+#!/usr/bin/perl -ls
+$_=shift;
+if (not defined $_) {die "Add link";}
+if ($_=~/youtu\.?be\.?.*\//)
+{my $title = `curl -silent -L $_ | egrep -o '>.*<\/tit' | cut -c2- | cut -d '<' -f 1`;
+my $user = `curl -silent -L $_ | egrep -o '\"\/user\/.*\"' | cut -c8- | cut  -d '\"' -f 1`;
+$user =~ s/\n//gi;
+print "test\n";
+print "$user - $title";}
+#elseif {$_=~/(i.)?imgur\.com }
+else {print "failed";}
