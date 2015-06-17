ca3e37c723803277eb50b6e14cb782bc8911ef5f
Sergey Shcherbakov <sj@404.pm>
2015-06-17 19:58:10 +0300

    minor improvements

--- forecast.pl
+++ forecast.pl
@@ -21,23 +21,26 @@ $city = shift;
 if (not defined $city) {die "Usage: $0 <city_name>\n";}
 
 system "curl -silent -L https://pogoda.yandex.ru/$city -o \"tmp\"";
+system "curl -silent -L https://p.ya.ru/$city -o \"tmp2\"";
 
 # Секция парсинга данных
-$title = `egrep -o 'title_level_1">.*<\/h1>' ./tmp | cut -c16- | cut -d '<' -f 1`;
+$title = `egrep -o 'title_level_1">.*<\/h1>' ./tmp | cut -d '>' -f 2 | cut -d '<' -f 1`;
 if ($title eq "Такой страницы не существует\n") {die "Города нет в базе данных\n";}
-$current = `egrep -o '_thermometer_type_now">.[0-9]+' ./tmp | cut -c24-`;
-$after = `egrep -o '_thermometer_type_after">.[0-9]+' ./tmp | cut -c26-`;
-$comment = `egrep -o 'current-weather__comment">[- абвгдеёжзиЙклмнопрстуфхцчшщЪыьэюя,]+' ./tmp | cut -c27-`;
+$current = `egrep -o '_thermometer_type_now">.[0-9]+' ./tmp | cut -d '>' -f 2`;
+$after = `egrep -o '_thermometer_type_after">.[0-9]+' ./tmp | cut -d '>' -f 2`;
+$comment = `egrep -o 'current-weather__comment">[- а-я,]+' ./tmp | cut -d '>' -f 2`;
 $wind_d = `egrep -o 'title="Ветер:.*' ./tmp | cut -c20- | cut -d '"' -f 1`;
-$wind_s = `egrep -o 'Ветер: <\/span>(Штиль| [0-9]+,[0-9] м\/с)' ./tmp | cut -c20-`;
-$pressure = `egrep -o 'n>[0-9]* мм рт. ст.' ./tmp | cut -c3-`;
-$humidity = `egrep -o 'n>[0-9]*%' ./tmp | cut -c3-`;
-$sunrise = `egrep -o 'д: <\/span>[0-9][0-9]:[0-9][0-9]' ./tmp | cut -c12-`;
-$sunset = `egrep -o 'т: <\/span>[0-9][0-9]:[0-9][0-9]' ./tmp | cut -c12-`;
-$forecast_day = `egrep -o 'item-dayname">[а-я]+' ./tmp | cut -c15-`;
-$forecast_day_t = `egrep -o 'Максимальная температура днём">.?[0-9]+[ а-я]*<\/div>' ./tmp | cut -c59- | cut -d '<' -f 1`;
-$forecast_night_t = `egrep -o 'Минимальная температура ночью">.?[0-9]+[ а-я]*<\/div>' ./tmp | cut -c59- | cut -d '<' -f 1`;
-$tomorrow_state= `egrep -o 'item-comment">[- а-я,ё]+' ./tmp | cut -c15`;
+$wind_s = `egrep -o 'Ветер: <\/span>(Штиль| [0-9]+,[0-9] м\/с)' ./tmp | cut -d '>' -f 2`;
+$pressure = `egrep -o 'n>[0-9]* мм рт. ст.' ./tmp | cut -d '>' -f 2`;
+$humidity = `egrep -o 'n>[0-9]*%' ./tmp | cut -d '>' -f 2`;
+$sunrise = `egrep -o 'д: <\/span>[0-9][0-9]:[0-9][0-9]' ./tmp | cut -d '>' -f 2`;
+$sunset = `egrep -o 'т: <\/span>[0-9][0-9]:[0-9][0-9]' ./tmp | cut -d '>' -f 2`;
+$forecast_day = `egrep -o 'item-dayname">[а-я]+' ./tmp | cut -d '>' -f 2`;
+$forecast_day_t = `egrep -o 'ём">.?[0-9]+' ./tmp | cut -d '>' -f 2`;
+$forecast_hour = `egrep -o 'th">[0-9][0-9]?' ./tmp2 | head | cut -d '>' -f 2`;
+$forecast_hour_t = `egrep -o 're">.?[0-9]+' ./tmp2 | head | cut -d '>' -f 2`;
+$forecast_night_t = `egrep -o 'ночью">.?[0-9]+' ./tmp | cut -d '>' -f 2`;
+$tomorrow_state= `egrep -o 'item-comment">[- а-я,ё]+' ./tmp | cut -d '>' -f 2`;
 
 if ($wind_s eq "Штиль") { $wind_s='0 м/с'; $wind_d='штиль'; }
 
@@ -57,15 +60,18 @@ $comment =~ s/\n//i;
 $after =~ s/\n/°C\t/i;
 $after =~ s/\n/°C/i;
 $forecast_day =~ s/\n/\t/gi;
-$forecast_day_t =~ s/ днем//gi;
+$forecast_hour =~ s/\n/ч\t/gi;
+$forecast_hour_t =~ s/\n/°C\t/gi;
+#$forecast_day_t =~ s/ днем//gi;
 $forecast_day_t =~ s/\n/°C\t/gi;
-$forecast_night_t =~ s/ ночью//gi;
+#$forecast_night_t =~ s/ ночью//gi;
 $forecast_night_t =~ s/\n/°C\t/gi;
 
 # Секция вывода
 print "$title\nСейчас \t\t$current, $comment\nВлажность: \t$humidity\nДавление: \t$pressure";
 print "Восход: \t$sunrise\nЗакат: \t\t$sunset\nВетер: \t\t$wind_s, $wind_d";
-print "\nБлижайший прогноз\n+0ч\t+6ч\t+12ч\n$current\t$after";
+#print "\nБлижайший прогноз\n+0ч\t+6ч\t+12ч\n$current\t$after";
+print "Ближайший прогноз:\n$forecast_hour\n$forecast_hour_t";
 print "\nПрогноз на следующие 9 дней";
 print "Время\t$forecast_day";
 print "День\t$forecast_day_t";
